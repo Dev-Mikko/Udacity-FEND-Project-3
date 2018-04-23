075d343835f1779d759ac1ae679d8bb98d2e13c6
Michele De Palma <35871747+Dev-Mikko@users.noreply.github.com>
2018-04-23 14:13:33 +0200

    Update the project
    Added new function and improved the design

--- README.md
+++ README.md
@@ -2,8 +2,27 @@
 
 **Creator**: Michele "*Dev_Mikko*" De Palma
 
+**How to install**:
+
+- Download this repository
+- Upload files on your web space or open index.html to show the game in your browser
+- To modify the score criteria, modify the scoreUpdate() function in "/js/app.js"
+
+**How to play**:
+
+Click on the cards to pair them. The timer starts when you try to do the first pairment.
+The game ends when you end the lifes or complete all pairings.
+The final score is time based. This is the score table:
+	- t < 60 sec: 3 stars
+	- 60 <= t < 90 sec: 2 stars
+	- 90 <= t < 105 sec: 1 star
+	- t >= 105 sec: 0 stars
+
 **Credits**:
 
 - Michele "*Dev_Mikko*" De Palma for the JS script and for some modify to CSS file
 - Udacity for the base code of the project
-- [Daniel "*Daneden*" Eden](http://daneden.me/) for the [animate.css](http://daneden.github.io/animate.css/) file
+- [Bootstrap](https://getbootstrap.com/) for "Font Awesome"
+- [Google fonts](https://fonts.google.com) for "Coda" and "Roboto" fonts
+- [Daniel "*Daneden*" Eden](http://daneden.me/) for some of his animations. Download the original file from here: [animate.css](http://daneden.github.io/animate.css/)
+- Shuffle function from [here](http://stackoverflow.com/a/2450976)
--- js/app.js
+++ js/app.js
@@ -1,45 +1,73 @@
 /*
- * Create a list that holds all of your cards
+ * Gaming variables & constants
  */
 
-const deckArea = document.querySelector('.deck');
+let cards = document.querySelectorAll('.deck .card');
 
-let cards = deckArea.querySelectorAll('.card');
-
-/*
- * Gaming variables
- */
-
-let unmatchedCards, moves, lifes;
+let unmatchedCards, moves, lifes, starsNumber, lifesNumber, t, scoreUp;
 
 const starsArea = document.querySelector('.stars');
 
 let star = starsArea.firstElementChild.outerHTML;
 
+const lifesArea = document.querySelector('.lifes');
+
+let life = lifesArea.firstElementChild.outerHTML;
+
 let movesNumberTxt = document.querySelector('.moves');
 
 let openedCards = [];
 
-/*
- * Restart game function
- */
-
 const restartBtn = document.querySelector('.restart');
 
-function restartGame() {
-	let numberStars = starsArea.childElementCount;
+let minutes = document.getElementById("minutes");
 
+let seconds = document.getElementById("seconds");
+
+let totalSeconds = 0;
+
+let modal = document.querySelector('.modal');
+
+let closeBtn = document.querySelector('.close');
+
+let tryAgainBtn = document.getElementById('tryAgain');
+
+let points = document.querySelector('#points');
+
+/*
+ * restartGame(): restarts the game and reset all gaming variables
+ */
+
+function restartGame() {
+	starsNumber = starsArea.childElementCount;
+	lifesNumber = lifesArea.childElementCount;
+
+	hideModal();
 	unmatchedCards = 16;
-	lifes = 3;
+	lifes = 10;
 	moves = 0;
 	movesNumberTxt.innerText = moves;
 
+	timerReset();
+	clearInterval(scoreUp);
+	
+	if(openedCards.length > 0) {
+		openedCards.pop();
+	}
+
 	cardShuffling();
 
-	for(let i = numberStars; i < 3; i++) {
-		if(numberStars < 3) {
+	for(let i = starsNumber; i < 3; i++) {
+		if(starsNumber < 3) {
 			let li = document.createElement('li');
 			starsArea.insertAdjacentHTML('afterbegin', star);
+		}
+	}
+
+	for(let i = lifesNumber; i < 10; i++) {
+		if(lifesNumber < 10) {
+			let li = document.createElement('li');
+			lifesArea.insertAdjacentHTML('afterbegin', life);
 		}
 	}
 
@@ -48,18 +76,18 @@ function restartGame() {
 	}
 }
 
+/*
+ * Enables the game reset throught the circular arrow button
+ */
+
 restartBtn.addEventListener('click', function() {
 	restartGame();
 });
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ * Shuffle(): shuffles an array or a list
+ * Shuffle function from http://stackoverflow.com/a/2450976
  */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
@@ -74,6 +102,10 @@ function shuffle(array) {
 
     return array;
 }
+
+/*
+ * cardShuffling(): using the shuffle() function, shuffles all the gaming card on the grid
+ */
 
 function cardShuffling() {
 	let cardClass = [];
@@ -91,29 +123,76 @@ function cardShuffling() {
 }
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * When all elements are loaded, starts the gaming functions 
  */
 
 document.addEventListener('DOMContentLoaded', function() {
 	restartGame();
-	
+	scoreUp = setInterval(scoreUpdate, 1000);
+
 	for(let card of cards) {
 		card.addEventListener('click', function() {
 			if(card.className === 'card') {
 				showCard(card);
 				addCardToList(card);
 				matchingControl();
+				timerStart();
 			}
 		});
 	}
 });
+
+/*
+ * Timer functions
+ * 	- timerUpdate(): updates the timer
+ *	- clock(val): converts the totalSeconds variable in a mm:ss timer
+ *	- timerStart(): stars the timer
+ *	- timerStop(): stops the timer
+ *	- timerreset(): resets the timer
+ */
+
+function timerUpdate() {
+	++totalSeconds;
+	seconds.innerHTML = clock(totalSeconds % 60);
+	minutes.innerHTML = clock(parseInt(totalSeconds / 60));
+}
+
+function clock(val) {
+	var valStr = val + "";
+	if (valStr.length < 2) {
+		return "0" + valStr;
+	} else {
+		return valStr;
+	}
+}
+
+function timerStart() {
+	if(moves === 0) {
+		t = setInterval(timerUpdate, 1000);
+	}
+}
+
+function timerStop() {
+	clearInterval(t);
+}
+
+function timerReset() {
+	timerStop();
+	totalSeconds = 0;
+	seconds.innerHTML = "00";
+	minutes.innerHTML = "00";
+}
+
+/*
+ * Gaming functions:
+ *	- showCard(card): shows the selected card
+ *	- addCardToList(card): adds the selected card to a stack of opened cards
+ *	- matchingControl(): verifies that the two previous selected cards are equal. At the end of this verify, empties the openedCards stack
+ *	- correctMatch(): holds the correct cards on the grid
+ *	- wrongMatch(): flips the wrong match and remove a life
+ *	- movesUpdate(), lifesUpdate(): updates the moves and lifes counter
+ *	- scoreUpdate(): updates the score using a time criteria
+ */
 
 function showCard(card) {
 	card.className = 'card open show animated bounceIn';
@@ -125,7 +204,7 @@ function addCardToList(card) {
 
 function matchingControl() {
 	let n = openedCards.length;
-
+	
 	if(n === 2) {
 		movesUpdate();
 		if(openedCards[0].firstElementChild.className === openedCards[1].firstElementChild.className) {
@@ -151,7 +230,8 @@ function correctMatch() {
 	unmatchedCards -= 2;
 	
 	if(unmatchedCards === 0) {
-		alert(`Congratulations! You have completed the game!\n\nFinal score: ${moves} moves`);
+		timerStop();
+		showModal();
 	}
 }
 
@@ -175,7 +255,12 @@ function wrongMatch() {
 				card.className = 'card wrong animated flipInY';
 			}
 		}
-		alert(`GAME OVER\n\nFinal score: ${moves}`);
+		timerStop();
+		starsNumber = starsArea.childElementCount;
+		for(let i = 0; i < starsNumber; i++) {
+			starsArea.firstElementChild.remove();
+		}
+		showModal();
 	}
 }
 
@@ -185,6 +270,65 @@ function movesUpdate() {
 }
 
 function lifesUpdate() {
-	starsArea.firstElementChild.remove();
+	lifesArea.firstElementChild.remove();
 	lifes--;
 }
+
+function scoreUpdate() {
+	if(totalSeconds === 60 || totalSeconds === 90 || totalSeconds === 105) {
+		starsArea.firstElementChild.remove();
+	}
+}
+
+/*
+ * Modal functions
+ *	- showModal(): shows the modal when the game ends
+ *	- hideModal(): hides the modal when a player click on the screen or click on the "X" button
+ */
+
+function showModal() {
+	starsNumber = starsArea.childElementCount;
+	let modalTitle = document.querySelector('.modal-title');
+	let modalText = document.querySelector('.modal-text');
+
+	modal.style.display = 'block';
+
+	if(lifes !== 0) {
+		modalTitle.innerText = 'Congratulations!';
+		modalText.innerText = `You won! Try to improve your score!\n\nMoves: ${moves} - Lifes: ${lifes}\nGame time: ${clock(parseInt(totalSeconds / 60))}:${clock(totalSeconds % 60)}`;
+		for(let i = 0; i < starsNumber; i++) {
+			points.insertAdjacentHTML('beforeend', star);
+		}
+	} else {
+		modalTitle.innerText = 'You lose...';
+		modalText.innerText = `You lose... But don't give up! You'll be more lucky next time!\n\nMoves: ${moves}\nGame time: ${clock(parseInt(totalSeconds / 60))}:${clock(totalSeconds % 60)}`;
+	}
+}
+
+function hideModal() {
+	starsNumber = points.childElementCount;
+
+	for(let i = 0; i < starsNumber; i++) {
+		points.firstElementChild.remove();
+	}
+
+	modal.style.display = 'none';
+}
+
+window.addEventListener('click', function(event) {
+    if(event.target === modal) {
+		hideModal();
+    }
+});
+
+closeBtn.addEventListener('click', function() {
+	hideModal();
+});
+
+/*
+ * Try again button function for the modal
+ */
+
+tryAgainBtn.addEventListener('click', function() {
+	restartGame();
+});
